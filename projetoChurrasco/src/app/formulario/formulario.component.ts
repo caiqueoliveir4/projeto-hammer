c3b4bf7c21339a831deac3b9c646e13cebb6dbb8
unknown <caiqueoliveir4>
2020-12-10 09:29:55 -0300

    finalizando o front

--- projetoChurrasco/src/app/formulario/formulario.component.ts
+++ projetoChurrasco/src/app/formulario/formulario.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-formulario',
-  templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+    selector: 'app-formulario',
+    templateUrl: './formulario.component.html',
+    styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+    dados = {
+        nomeParticipante: "",
+        churrasco: false,
+        bebida:  false,
+        possuiConvidado: false,
+        convidado: {
+            nome: "",
+            churrasco: false,
+            bebida: false
+        }
+    };
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+
+    ngOnInit(): void {
+    }
+
+    cadastrar(): void {
+        console.log("Dados", this.dados);
+    }
 
 }
--- projetoChurrasco/src/app/lista/lista.component.ts
+++ projetoChurrasco/src/app/lista/lista.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-lista',
-  templateUrl: './lista.component.html',
-  styleUrls: ['./lista.component.css']
+    selector: 'app-lista',
+    templateUrl: './lista.component.html',
+    styleUrls: ['./lista.component.css']
 })
 export class ListaComponent implements OnInit {
 
-  constructor() { }
+    listaCompleta = [
+        {
+            nome: "Teiji",
+            churrasco: "Sim",
+            bebida: "Sim",
+            possuiAcompanhante: "Sim",
+            valor: 200
+        },
+        { 
+            nome: "Caique", 
+            churrasco: "Sim", 
+            bebida: "Sim", 
+            possuiAcompanhante: "Sim", 
+            valor: 200 
+        },
+        { 
+            nome: "Wesley", 
+            churrasco: "Sim", 
+            bebida: "Sim", 
+            possuiAcompanhante: "Sim", 
+            valor: 200 
+        },
+        { 
+            nome: "Daia", 
+            churrasco: "Sim", 
+            bebida: "Sim", 
+            possuiAcompanhante: "Sim", 
+            valor: 200 
+        }
+    ];
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+
+    }
 
 }
